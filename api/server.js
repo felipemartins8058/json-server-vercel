ec0007fe6efcba53e5782863b98b6b31675130e1
Felipe Martins <felipecosmemartins10@gmail.com>
2024-05-09 14:25:42 -0300

    cors

--- api/server.js
+++ api/server.js
@@ -23,7 +23,7 @@ server.use(jsonServer.rewriter({
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
+    res.header('Access-Control-Allow-Origin', 'https://mapahogrefe.netlify.app/'); // Allow any origin
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
